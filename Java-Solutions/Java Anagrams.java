9f90178abe082e0c31480de2e2feebdd00a507fe
V.MANOGNA <65440296+manognachowdary7@users.noreply.github.com>
2024-06-07 20:53:13 +0530

    Create Java Anagrams.java

---
+++ Java-Solutions/Java Anagrams.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        if(a.length() != b.length())
+            return false;
+            
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(char letter : a.toCharArray()){
+            map.put(letter, map.getOrDefault(letter, 0) + 1);
+        }
+        for(char letter : b.toCharArray()){
+            if(!map.containsKey(letter) || map.get(letter) == 0)
+                return false;
+            else
+                map.put(letter, map.get(letter) - 1);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+    }
+}
