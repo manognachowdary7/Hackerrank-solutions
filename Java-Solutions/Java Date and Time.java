41ad87f133c6d3eae2d38f280f687a306f6cfa41
V.MANOGNA <65440296+manognachowdary7@users.noreply.github.com>
2024-06-07 20:43:58 +0530

    Create Java Date and Time.java

---
+++ Java-Solutions/Java Date and Time.java
@@ -0,0 +1,13 @@
+
+
+class Result {
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, month-1);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        cal.set(Calendar.YEAR, year);
+        String dayofweek = cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
+        return dayofweek;
+    }
+
+}
